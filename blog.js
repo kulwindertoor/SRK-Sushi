0d9324249e28291f68ed720c1f89d45b61b9dde3
Kulwinder Singh Toor <kulwinder@moscowmail.com>
2020-12-30 16:54:27 +0100

    Create blog.js

---
+++ blog.js
@@ -0,0 +1,56 @@
+function selectFirst(){
+      document.getElementById('definition').innerHTML = 'The design is useful and marketable to people with diverse abilities.';
+      document.getElementById('item-1').innerHTML = 'Provide the same means of use for all users: identical whenever possible; equivalent when not.';
+      document.getElementById('item-2').innerHTML = 'Avoid segregating or stigmatizing any users.';
+      document.getElementById('item-3').innerHTML = 'Provisions for privacy, security, and safety should be equally available to all users.';
+      document.getElementById('item-4').innerHTML = 'Make the design appealing to all users.';
+}
+
+function selectSecond(){
+    document.getElementById('definition').innerHTML = 'The design accommodates a wide range of individual preferences and abilities.';
+    document.getElementById('item-1').innerHTML = 'Provide choice in methods of use.';
+    document.getElementById('item-2').innerHTML = 'Accommodate right- or left-handed access and use.';
+    document.getElementById('item-3').innerHTML = 'Facilitate the users accuracy and precision.';
+    document.getElementById('item-4').innerHTML = 'Provide adaptability to the users pace.';
+}
+
+function selectThird(){
+    document.getElementById('definition').innerHTML = 'Use of the design is easy to understand, regardless of the users experience, knowledge, language skills, or current concentration level.';
+    document.getElementById('item-1').innerHTML = 'Eliminate unnecessary complexity.';
+    document.getElementById('item-2').innerHTML = 'Be consistent with user expectations and intuition.';
+    document.getElementById('item-3').innerHTML = 'Accommodate a wide range of literacy and language skills.';
+    document.getElementById('item-4').innerHTML = 'Arrange information consistent with its importance.';
+}
+
+function selectFourth(){
+    document.getElementById('definition').innerHTML = 'The design communicates necessary information effectively to the user, regardless of ambient conditions or the users sensory abilities.';
+    document.getElementById('item-1').innerHTML = 'Use different modes (pictorial, verbal, tactile) for redundant presentation of essential information.';
+    document.getElementById('item-2').innerHTML = 'Provide adequate contrast between essential information and its surroundings.';
+    document.getElementById('item-3').innerHTML = 'Maximize "legibility" of essential information.';
+    document.getElementById('item-4').innerHTML = 'Differentiate elements in ways that can be described (i.e., make it easy to give instructions or directions).';
+}
+
+function selectFifth(){
+    document.getElementById('definition').innerHTML = 'The design minimizes hazards and the adverse consequences of accidental or unintended actions.';
+    document.getElementById('item-1').innerHTML = 'Arrange elements to minimize hazards and errors: most used elements, most accessible; hazardous elements eliminated, isolated, or shielded.';
+    document.getElementById('item-2').innerHTML = 'Provide warnings of hazards and errors.';
+    document.getElementById('item-3').innerHTML = 'Provide fail safe features.';
+    document.getElementById('item-4').innerHTML = 'Discourage unconscious action in tasks that require vigilance.';
+}
+
+function selectSixth(){
+    document.getElementById('definition').innerHTML = 'The design can be used efficiently and comfortably and with a minimum of fatigue.';
+    document.getElementById('item-1').innerHTML = 'Allow user to maintain a neutral body position.';
+    document.getElementById('item-2').innerHTML = 'Use reasonable operating forces.';
+    document.getElementById('item-3').innerHTML = 'Minimize repetitive actions.';
+    document.getElementById('item-4').innerHTML = 'Minimize sustained physical effort.';
+}
+
+function selectSeventh(){
+    document.getElementById('definition').innerHTML = 'Appropriate size and space is provided for approach, reach, manipulation, and use regardless of users body size, posture, or mobility.';
+    document.getElementById('item-1').innerHTML = 'Provide a clear line of sight to important elements for any seated or standing user.';
+    document.getElementById('item-2').innerHTML = 'Make reach to all components comfortable for any seated or standing user.';
+    document.getElementById('item-3').innerHTML = 'Accommodate variations in hand and grip size.';
+    document.getElementById('item-4').innerHTML = 'Provide adequate space for the use of assistive devices or personal assistance.';
+}
+   
